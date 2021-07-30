2b145203ec0b3d3efa50066bf56170b9477dd735
Sudheer Mylapur <sudheer.mylapur@gmail.com>
2021-07-31 11:04:02 +1200

    bring back the code

---
+++ src/main/java/TestCases/BuggyHomePageTest.java
@@ -0,0 +1,43 @@
+package TestCases;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import PageClasses.BuggyHomePage;
+import PageClasses.UserRegistration;
+import baseClasses.BaseTestClass;
+import baseClasses.PageBaseClass;
+
+public class BuggyHomePageTest extends BaseTestClass{
+	
+	BuggyHomePage buggyHomePage;
+	UserRegistration userRegistration;
+	
+	@Test
+	public void buggyHomePageTest() {
+		
+		logger = report.createTest("Launching Buggy Cars Rating");
+		//better invoke to be insdide try
+		invokeBrowser("Chrome");
+		PageBaseClass pageBase = new PageBaseClass(driver, logger);
+		PageFactory.initElements(driver, pageBase);
+		buggyHomePage = pageBase.OpenApplication();
+		buggyHomePage.getTitle("Buggy Cars Rating");
+		buggyHomePage.verifyHomePageInfo();
+	}
+	
+	@Test
+	public void userRegistration() {
+		
+		logger = report.createTest("Register a user to Buggy Cars Rating");
+		
+		invokeBrowser("Chrome");
+		PageBaseClass pageBase = new PageBaseClass(driver, logger);
+		PageFactory.initElements(driver, pageBase);
+		buggyHomePage = pageBase.OpenApplication();
+		userRegistration = buggyHomePage.clickRegistrationLink();
+		userRegistration.registerUser();
+		
+	}
+
+}
---
+++ src/main/java/baseClasses/PageBaseClass.java
@@ -0,0 +1,145 @@
+package baseClasses;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.xmlbeans.impl.xb.xsdschema.All;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
+import PageClasses.BuggyHomePage;
+//import PageClasses.LandingPage;
+import utilities.DateUtil;
+
+public class PageBaseClass extends BaseTestClass {
+
+	public ExtentTest logger;
+
+	public PageBaseClass(WebDriver driver, ExtentTest logger) {
+		this.driver = driver;
+		this.logger = logger;
+	}
+
+	/****************** OpenApplication ***********************/
+	public BuggyHomePage OpenApplication() {
+		logger.log(Status.INFO, "Opening the WebSite");
+		driver.get("https://buggy.justtestit.org/");
+		logger.log(Status.PASS, "Successfully Opened the https://buggy.justtestit.org/");
+		BuggyHomePage buggyHomePage = new BuggyHomePage(driver, logger);
+		PageFactory.initElements(driver, buggyHomePage);
+		return buggyHomePage;
+	}
+	
+	/****************** Get Page Title ***********************/
+	public void getTitle(String expectedTitle) {
+		try {
+			Assert.assertEquals(driver.getTitle(), expectedTitle);
+			reportPass("Actual Title : " + driver.getTitle() + " - equals to Expected Title : " + expectedTitle);
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+
+	}
+	
+	
+	/****************** Verify Element is Present ***********************/
+	public void verifyElementIsDisplayed(WebElement webElement){
+		try {
+			if(webElement.isDisplayed()){
+				reportPass("Webelement " + webElement + "is Displayed");
+			}else {
+				reportFail("Webelement " + webElement + "has not appeared");
+			}
+			
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+		
+	}
+	
+	
+	/****************** Verify Element Text ***********************/
+	public void verifyElementTextIsCorrect(WebElement webElement, String webElementText){
+		try {
+			String text = webElement.getText();
+			System.out.println("----" + text + "----");
+			System.out.println("----" + webElementText + "----");
+			if(text.equals(webElementText)){
+				reportPass("Webelement text " + " for " + webElement.getText() + " text matches " + webElementText);
+			}else {
+				reportFail("Webelement text " + " for " + webElement.getText() + " text does not match " + webElementText);
+			}
+			
+		} catch (Exception e) {
+			reportFail(e.getMessage());
+		}
+		
+	}	
+
+	/****************** Reporting Functions ***********************/
+	public void reportFail(String reportString) {
+		logger.log(Status.FAIL, reportString);
+		takeScreenShotOnFailure();
+		Assert.fail(reportString);
+	}
+
+	public void reportPass(String reportString) {
+		logger.log(Status.PASS, reportString);
+	}
+
+	/****************** Capture Screen Shot ***********************/
+	public void takeScreenShotOnFailure() {
+		TakesScreenshot takeScreenShot = (TakesScreenshot) driver;
+		File sourceFile = takeScreenShot.getScreenshotAs(OutputType.FILE);
+
+		File destFile = new File(System.getProperty("user.dir") + "/ScreenShots/" + DateUtil.getTimeStamp() + ".png");
+		try {
+			FileUtils.copyFile(sourceFile, destFile);
+			logger.addScreenCaptureFromPath(
+					System.getProperty("user.dir") + "/ScreenShots/" + DateUtil.getTimeStamp() + ".png");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	/***************** Wait Functions in Framework *****************/
+	public void waitForPageLoad() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		int i = 0;
+		while (i != 10) {
+			String pageState = (String) js.executeScript("return document.readyState;");
+			if (pageState.equals("complete")) {
+				break;
+			} else {
+				waitLoad(1);
+			}
+		}
+
+		waitLoad(2);
+
+		i = 0;
+		while (i != 10) {
+			Boolean jsState = (Boolean) js.executeScript("return window.jQuery != undefined && jQuery.active == 0;");
+			if (jsState) {
+				break;
+			} else {
+				waitLoad(1);
+			}
+		}
+	}
+
+}
